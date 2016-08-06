427bba34a7a7d3efa881fbaf0cae7fdbbf1af962
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-08-06 23:19:07 +0200

    cleanup tester

--- Tester1/Program.cs
+++ Tester1/Program.cs
@@ -13,66 +13,39 @@ namespace PerformanceTest
         static void Main(string[] args)
         {
             var count = 1000000;
-            Console.WriteLine("start test with {0:N0} messages", count);
-            Stopwatch sw = Stopwatch.StartNew();
 
-            TestLoggerHierarchy(count);
+            //gdc test (now disabled)
+            string jobId = System.Guid.NewGuid().ToString();
+            NLog.GlobalDiagnosticsContext.Set("jobId", jobId);
+
+            //getting logger not part of the test
+            var logger = LogManager.GetLogger("logger");
+            Console.WriteLine("start test with {0:N0} messages", count);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            var paralllel = 1;
+            for (var i = 0; i < paralllel; i++)
+            {
+                WriteMessages(logger, count);
+            }
             sw.Stop();
+
             Console.WriteLine("{2:N} messages. Time taken: {0:N}ms. {1:N} / sec", sw.Elapsed.TotalMilliseconds,
                 ((double)count / sw.Elapsed.TotalMilliseconds) * 1000, count);
             Console.ReadKey();
         }
 
-        private static void TestLoggerHierarchy(int count)
+        private static void WriteMessages(Logger logger, int count)
         {
-            for (var i = 0; i < 1; i++)
+            logger.Info("Log Started");
+
+            for (var line = 0; line < count; line++)
             {
-                //{
-
-                //    string folderName = @"c:\temp\Log";
-                //    string jobId = System.Guid.NewGuid().ToString();
-                //    jobId = jobId + "\\MIPS";
-                //    string pathString = System.IO.Path.Combine(folderName, jobId);
-                //    System.IO.Directory.CreateDirectory(pathString);
-
-                //    string fileName = "Mipslogger.log";
-                //    pathString = System.IO.Path.Combine(pathString, fileName);
-                //    using (System.IO.FileStream fs = System.IO.File.Create(pathString))
-                //    {
-
-                //    }
-                //    using (System.IO.StreamWriter file =
-                //   new System.IO.StreamWriter(pathString))
-                //    {
-                //        for (var line = 0; line < (1000000 + (i * 100)); line++)
-                //          for (var line = 0; line < (1000000 + (1 * 100)); line++)
-                //        {
-                //            file.WriteLine("MIP : " + line);
-                //        }
-                //    }
-                //}
-
-
-
-                {
-                    string jobId = System.Guid.NewGuid().ToString();
-                    NLog.GlobalDiagnosticsContext.Set("jobId", jobId);
-
-                    var mipLogger = LogManager.GetLogger("mipLogger");
-
-                    mipLogger.Info("MIP Started");
-
-                    for (var line = 0; line < (count + (i * 100)); line++)
-                    {
-                        mipLogger.Info("MIP : {0}" + line);
-                        // mipLogger.Info(new LogEventInfo(LogLevel.Info, "mipLogger", "MIP : " + line));
-                    }
-                    LogManager.Flush();
-                    mipLogger.Info("MIP Finished");
-                }
-
+                logger.Debug("Line : " + line);
+                // mipLogger.Info(new LogEventInfo(LogLevel.Info, "mipLogger", "MIP : " + line));
             }
+            LogManager.Flush();
+            logger.Info("Log Finished");
         }
-
     }
 }
