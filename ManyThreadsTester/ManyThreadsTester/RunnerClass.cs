ec170987d83533cc03678cc8e755552e0a0e55f6
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-10-10 22:09:37 +0200

    Add test program with many threads

---
+++ ManyThreadsTester/ManyThreadsTester/RunnerClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NLogPerfTest
+{
+    public class RunnerClass
+    {
+        public static void Main1()
+        {
+            CpuUsage cpu = new CpuUsage();
+            double usage = cpu.GetUsage();
+            Console.WriteLine(usage.ToString());
+        }
+    }
+}
---
+++ ManyThreadsTester/ManyThreadsTester/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace ParallelTask
+{
+    public enum Status
+    {
+        St1 = 0,
+        St2 = 2,
+        St3 = 3
+    }
+}
