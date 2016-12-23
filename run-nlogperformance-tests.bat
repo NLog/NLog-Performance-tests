e7c32f0ca135ddd7b35a7d6baed8c0356b41266d
Rolf Kristensen <sweaty1@hotmail.com>
2016-12-23 11:37:23 +0100

    NLogPerformance - MultiFile splits the workload on multiple targets, but make the targets take smaller batches.

--- run-nlogperformance-tests.bat
+++ run-nlogperformance-tests.bat
@@ -10,6 +10,8 @@ NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 4  16 1 >> perfor
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 8  16 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 16 16 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 60 16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe MultiLogger 10000000 1  16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe MultiLogger 10000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  8  1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  128 1 >> performance-log.log
