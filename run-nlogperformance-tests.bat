7f65497044268cf6f772be504d7320cd687622d6
Rolf Kristensen <sweaty1@hotmail.com>
2017-01-13 09:51:52 +0100

    Removed parameter tweaking, as NLog 4.4.2 works out of the box

--- run-nlogperformance-tests.bat
+++ run-nlogperformance-tests.bat
@@ -1,8 +1,7 @@
-NLogPerformance\bin\Release\NLogPerformance.exe PerfLogger 10000000 1  16 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe PerfLogger 10000000 1  128 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe PerfLogger 15000000 1  16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe PerfLogger 15000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe PerfSyncLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe PerfSyncLogger 10000000 1  128 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 1  8  1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 2  16 1 >> performance-log.log
@@ -10,9 +9,9 @@ NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 4  16 1 >> perfor
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 8  16 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 16 16 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 60 16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe PerfArchiveLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe MultiLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe MultiLogger 10000000 1  128 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  8  1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  128 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 2  16 1 >> performance-log.log
@@ -20,7 +19,6 @@ REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 4  16 1 >
 REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 8  16 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 16 16 1 >> performance-log.log
 REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 60 16 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 1  8  1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 2  16 1 >> performance-log.log
