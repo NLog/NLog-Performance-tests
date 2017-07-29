eaa6df3e270e0ec4929966a097a1f933a14c352f
Rolf Kristensen <sweaty1@hotmail.com>
2017-07-29 23:51:29 +0200

    NLogPerformance - Added message arguments option

--- run-nlogperformance-tests.bat
+++ run-nlogperformance-tests.bat
@@ -5,24 +5,29 @@ NLogPerformance\bin\Release\NLogPerformance.exe PerfSyncLogger 10000000 1  128 1
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 2  16 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 4  16 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 8  16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 16 16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 3000000 60 16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 4  16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 8  16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 16 16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe Logger 10000000 60 16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe PerfArchiveLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe MultiLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe MultiLogger 10000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 1  128 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 2  16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 4  16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 8  16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 16 16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 3000000 60 16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 10000000 2  16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 15000000 4  16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 15000000 8  16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 15000000 16 16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SyncLogger 15000000 60 16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 1  16 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 1  128 1 >> performance-log.log
 NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 2  16 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 3000000 4  16 1 >> performance-log.log
-NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 3000000 8  16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 3000000 16 16 1 >> performance-log.log
-REM NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 3000000 60 16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 4  16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 8  16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 16 16 1 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SimpleLogger 10000000 60 16 1 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe SimpleLevel 20000000 1 128 1 2 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe SimpleLevel 20000000 4 128 1 2 >> performance-log.log
+NLogPerformance\bin\Release\NLogPerformance.exe SimpleLevel 20000000 8 128 1 2 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SimpleLevel 20000000 16 128 1 2 >> performance-log.log
+REM NLogPerformance\bin\Release\NLogPerformance.exe SimpleLevel 20000000 60 128 1 2 >> performance-log.log
