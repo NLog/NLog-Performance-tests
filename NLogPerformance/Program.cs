de88fa0b6bf90d753290293e9bd2a5ae27a93851
Rolf Kristensen <sweaty1@hotmail.com>
2022-08-13 12:43:27 +0200

    Updated to NLog 5.0.2 and added 64 bit check

--- NLogPerformance/Program.cs
+++ NLogPerformance/Program.cs
@@ -5,7 +5,7 @@ namespace NLogPerformance
     static class Program
     {
         private static string _loggerName = "SimpleLogger";
-        private static int _messageCount = 10000000;
+        private static int _messageCount = 5000000;
         private static int _threadCount = 1;
         private static int _messageSize = 30;
         private static int _messageArgCount = 0;
